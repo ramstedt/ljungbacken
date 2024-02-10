ea3fa9754bb563fb0f09ad66135a3112b22079d0
Emma Ramstedt <71063351+emmaramstedt@users.noreply.github.com>
2024-02-10 15:00:03 +0100

    footer component

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.footer`
+  margin-top: auto;
+  width: 100vw;
+  background: grey;
+  display: flex;
+`;
+
+const ContactWrapper = styled.div``;
+
+const MapsWrapper = styled.div`
+  width: 300px;
+  height: 300px;
+`;
+//https://www.maps.ie/create-google-map/
+export default function Footer() {
+  return (
+    <Wrapper>
+      <ContactWrapper>Kontakt</ContactWrapper>
+      <MapsWrapper>
+        <iframe
+          width='100%'
+          height='100%'
+          frameborder='0'
+          marginheight='0'
+          marginwidth='0'
+          src='https://maps.google.com/maps?25&amp;hl=sv&amp;q=Brunnsparken,%20411%2006%20G%C3%B6teborg+(brunnsparken)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed'
+        ></iframe>
+      </MapsWrapper>
+    </Wrapper>
+  );
+}
--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,6 +1,13 @@
 import Navbar from '@/components/Navbar/Navbar';
 import Head from 'next/head';
 import styled from 'styled-components';
+import Footer from '@/components/Footer/Footer';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100lvh;
+`;
 
 const Main = styled.main`
   padding-top: 68px;
@@ -8,7 +15,7 @@ const Main = styled.main`
 
 export default function Layout({ children }) {
   return (
-    <>
+    <Wrapper>
       <Head>
         <title>Ljungbacken</title>
         <link rel='icon' href='' />
@@ -18,6 +25,7 @@ export default function Layout({ children }) {
       </Head>
       <Navbar />
       <Main>{children}</Main>
-    </>
+      <Footer />
+    </Wrapper>
   );
 }
